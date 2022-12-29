25a46e7dfa0a68ade7bc74e18bbc2d60596c4743
mitkalp <mit_shlok@yahoo.co.in>
2022-12-29 15:53:18 +0000

    changed code

--- src/main/java/com/qa/opencart/pages/DemoImp.java
+++ src/main/java/com/qa/opencart/pages/DemoImp.java
@@ -2,12 +2,14 @@ package com.qa.opencart.pages;
 
 public class DemoImp {
 
-<<<<<<< HEAD
-=======
-	String name = "prashant";
-	int total = 100;
+public void order() {
+	String name = "naveen";
+	int p = 100;
+	int id = 102;
+	
+}
 	
 	
 	
->>>>>>> e629b4d99399189ba9fb9e23ca552d1a42da0199
+
 }
